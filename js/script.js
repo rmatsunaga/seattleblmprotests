3e1eea753ef087ddd1c23775813dec8e9fc00863
matsunagar97@gmail.com <matsunagar97@gmail.com>
2020-06-10 01:48:00 -0700

    added in comments for organization

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// Setting up your map
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Type of map and attributes of map
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -7,6 +10,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoicmV5bSIsImEiOiJja2I5M3J1ZmwwYWN2MzJvMHdreHhxaDJwIn0.olCVAYUagJ04orqE_BZtQA'
 }).addTo(mymap);
+
+// Markers, circles and polygons
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
@@ -19,6 +24,20 @@ var polygon = L.polygon([
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(mymap);
+
+// Popups
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
+var popup = L.popup();
+
+
+// Events
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
